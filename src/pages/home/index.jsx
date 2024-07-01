cc314a0ed5f574afd762e39d51955f24d2889dc8
rodri <rodrigo.llancao@pregrado.uoh.cl>
2024-07-01 03:06:41 -0400

    verify google

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -34,7 +34,9 @@ export default function Home() {
         "static/images/marcas7.png",
     ];
     return (
+        
         <>
+        <meta name="google-site-verification" content="7HYNzFrSrKTNZa9AAHrsEqqawNWYf-q7Nltc0qJM__Y" />
         <div style={{ backgroundColor: "#FDF5E6" }}> {/* Cambia este color por el que desees */}
 
         <Carrrusel slides={slides} />
